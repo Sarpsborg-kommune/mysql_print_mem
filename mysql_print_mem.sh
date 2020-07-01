3c36468e5b4e08362d02bf9d5e06264b85b25642
Jens Torgeir Solaugsten <torger@solaugsten.no>
2020-07-01 10:00:21 +0200

    Corrected lineshift

--- mysql_print_mem.sh
+++ mysql_print_mem.sh
@@ -11,14 +11,8 @@ VAR[$1]=$2
 END {
 MAX_CONN = VAR["max_connections"]
 MAX_USED_CONN = VAR["Max_used_connections"]
-BASE_MEM=VAR["key_buffer_size"] + VAR["query_cache_size"] +
-VAR["innodb_buffer_pool_size"] +
-VAR["innodb_additional_mem_pool_size"] +
-VAR["innodb_log_buffer_size"]
-MEM_PER_CONN=VAR["read_buffer_size"] + VAR["read_rnd_buffer_size"] +
-VAR["sort_buffer_size"] + VAR["join_buffer_size"] +
-VAR["binlog_cache_size"] + VAR["thread_stack"] +
-VAR["tmp_table_size"]
+BASE_MEM=VAR["key_buffer_size"] + VAR["query_cache_size"] + VAR["innodb_buffer_pool_size"] + VAR["innodb_additional_mem_pool_size"] + VAR["innodb_log_buffer_size"]
+MEM_PER_CONN=VAR["read_buffer_size"] + VAR["read_rnd_buffer_size"] + VAR["sort_buffer_size"] + VAR["join_buffer_size"] + VAR["binlog_cache_size"] + VAR["thread_stack"] + VAR["tmp_table_size"]
 MEM_TOTAL_MIN=BASE_MEM + MEM_PER_CONN*MAX_USED_CONN
 MEM_TOTAL_MAX=BASE_MEM + MEM_PER_CONN*MAX_CONN
 
