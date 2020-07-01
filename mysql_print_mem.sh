def0c063a869b06c52902c402abe93576b9190b0
Jens Torgeir Solaugsten <torger@solaugsten.no>
2020-07-01 09:57:16 +0200

    Modifications

--- mysql_print_mem.sh
+++ mysql_print_mem.sh
@@ -9,5 +9,19 @@ mysql -u root -p -e "show variables; show status" | awk '
 VAR[$1]=$2
 }
 END {
+MAX_CONN = VAR["max_connections"]
+MAX_USED_CONN = VAR["Max_used_connections"]
+BASE_MEM=VAR["key_buffer_size"] + VAR["query_cache_size"] +
+VAR["innodb_buffer_pool_size"] +
+VAR["innodb_additional_mem_pool_size"] +
+VAR["innodb_log_buffer_size"]
+MEM_PER_CONN=VAR["read_buffer_size"] + VAR["read_rnd_buffer_size"] +
+VAR["sort_buffer_size"] + VAR["join_buffer_size"] +
+VAR["binlog_cache_size"] + VAR["thread_stack"] +
+VAR["tmp_table_size"]
+MEM_TOTAL_MIN=BASE_MEM + MEM_PER_CONN*MAX_USED_CONN
+MEM_TOTAL_MAX=BASE_MEM + MEM_PER_CONN*MAX_CONN
 
+printf "+--------------------------------+-------------------------------------+\n"
+printf "| %40s | %15.3f MB |\n", "key_buffer_size, VAR["key_buffer_size"]/1048576
 }'
