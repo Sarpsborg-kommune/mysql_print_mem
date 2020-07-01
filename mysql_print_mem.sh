17f88ec4dada42ea5c236848e995003586daba32
Jens Torgeir Solaugsten <torger@solaugsten.no>
2020-07-01 10:21:55 +0200

    Error corrections

--- mysql_print_mem.sh
+++ mysql_print_mem.sh
@@ -16,15 +16,15 @@ MEM_PER_CONN=VAR["read_buffer_size"] + VAR["read_rnd_buffer_size"] + VAR["sort_b
 MEM_TOTAL_MIN=BASE_MEM + MEM_PER_CONN*MAX_USED_CONN
 MEM_TOTAL_MAX=BASE_MEM + MEM_PER_CONN*MAX_CONN
 
-printf "+------------------------------------+---------------------+\n"
+printf "+-------------------------------------+--------------------+\n"
 printf "| %35s | %15.3f MB |\n", "key_buffer_size", VAR["key_buffer_size"]/1048576
 printf "| %35s | %15.3f MB |\n", "query_cache_size", VAR["query_cache_size"]/1048576
 printf "| %35s | %15.3f MB |\n", "innodb_buffer_pool_size", VAR["innodb_buffer_pool_size"]/1048576
 printf "| %35s | %15.3f MB |\n", "innodb_additional_mem_pool_size", VAR["innodb_additional_mem_pool_size"]/1048576
 printf "| %35s | %15.3f MB |\n", "innodb_log_buffer_size", VAR["innodb_log_buffer_size"]/1048576
-printf "+------------------------------------+---------------------+\n"
+printf "+-------------------------------------+--------------------+\n"
 printf "| %35s | %15.3f MB |\n", "BASE MEMORY", BASE_MEM/1048576
-printf "+------------------------------------+---------------------+\n"
+printf "+-------------------------------------+--------------------+\n"
 printf "| %35s | %15.3f MB |\n", "sort_buffer_size", VAR["sort_buffer_size"]/1048576
 printf "| %35s | %15.3f MB |\n", "read_buffer_size", VAR["read_buffer_size"]/1048576
 printf "| %35s | %15.3f MB |\n", "read_rnd_buffer_size", VAR["read_rnd_buffer_size"]/1048576
@@ -32,13 +32,13 @@ printf "| %35s | %15.3f MB |\n", "join_buffer_size", VAR["join_buffer_size"]/104
 printf "| %35s | %15.3f MB |\n", "thread_stack", VAR["thread_stack"]/1048576
 printf "| %35s | %15.3f MB |\n", "binlog_cache_size", VAR["binloag_cache_size"]/1048576
 printf "| %35s | %15.3f MB |\n", "tmp_table_size", VAR["tmp_table_size"]/1048576
-printf "+------------------------------------+---------------------+\n"
+printf "+-------------------------------------+--------------------+\n"
 printf "| %35s | %15.3f MB |\n", "MEMORY PER CONNCECTION", MEM_PER_CONN/1048576
-printf "+------------------------------------+---------------------+\n"
-printf "| %35s | %15.3f MB |\n", "Max_used_connections", MAX_USED_CONN
-printf "| %35s | %15.3f MB |\n", "max_connections", MAX_CONN
-printf "+------------------------------------+---------------------+\n"
+printf "+-------------------------------------+--------------------+\n"
+printf "| %35s | %18d |\n", "Max_used_connections", MAX_USED_CONN
+printf "| %35s | %18d |\n", "max_connections", MAX_CONN
+printf "+-------------------------------------+--------------------+\n"
 printf "| %35s | %15.3f MB |\n", "TOTAL (MIN)", MEM_TOTAL_MIN/1048576
 printf "| %35s | %15.3f MB |\n", "TOTAL (MAX)", MEM_TOTAL_MAX/1048576
-printf "+------------------------------------+---------------------+\n"
+printf "+-------------------------------------+--------------------+\n"
 }'
